413fd770f4acd714f648840d7a0a1a0e24da4d54
Leiner <leiner_0304@hotmail.com>
2017-02-21 21:33:25 -0500

    Aplicación sin validación terminada

--- app/src/main/java/com/example/android/holamundo/Principal.java
+++ app/src/main/java/com/example/android/holamundo/Principal.java
@@ -2,12 +2,27 @@ package com.example.android.holamundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
+
+    private EditText cajaNombre;
+    private TextView cajaMensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        cajaNombre = (EditText)findViewById(R.id.txtNombre);
+        cajaMensaje = (TextView)findViewById(R.id.lblMensaje);
+    }
+
+    public void saludar(View v){
+        String aux;
+        aux = cajaNombre.getText().toString();
+        cajaMensaje.setText(getResources().getString(R.string.parte_saludo)+" "+aux);
     }
 }
